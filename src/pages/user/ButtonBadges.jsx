3c6c604c1a2faee7adcbb903d67fc7e2fa41277d
ashaunni1998 <ashaunnimca@gmail.com>
2025-08-04 14:58:55 +0530

    next commit button

--- src/pages/user/ButtonBadges.jsx
+++ src/pages/user/ButtonBadges.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Link } from 'react-router-dom';
+
 
 const ButtonBadges = () => {
   return (
@@ -49,9 +51,13 @@ const ButtonBadges = () => {
                 />
                 <h4 style={{ marginTop: "12px", fontSize: "16px", fontWeight: "600" }}>{item.title}</h4>
                 <p style={{ color: "#555", fontSize: "14px", margin: "4px 0" }}>{item.desc}</p>
-                <a href={item.link} style={{ color: "#00704A", fontSize: "14px", textDecoration: "none", fontWeight: "500" }}>
-                  Shop {item.title}
-                </a>
+               <Link
+  to={item.link}
+  style={{ color: "#00704A", fontSize: "14px", textDecoration: "none", fontWeight: "500" }}
+>
+  Shop {item.title}
+</Link>
+
               </div>
             ))}
           </div>
